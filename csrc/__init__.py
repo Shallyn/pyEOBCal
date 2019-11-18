bfb80f658c1615349c7d7445ae9c4127ac7426c8
Shallyn <shallyn.liu@foxmail.com>
2019-11-18 21:57:46 +0800

    init

--- csrc/__init__.py
+++ csrc/__init__.py
@@ -10,6 +10,8 @@
 
 from WTestLib.Utils import cmd_stdout_cev, CEV 
 from pathlib import Path
+import time
+from numpy import random as rd
 
 LOC = Path(__file__).parent
 EXE = LOC / 'MAIN'
@@ -23,6 +25,11 @@ def ConstructCMD(exe = EXE, **kwargs):
             CMD.append(f'--{opt}={val}')
     return ' '.join(CMD)
 
+def get_random_jobtag():
+    ttag = int(time.time() / 1000)
+    rdtag = int(rd.randn()*ttag)
+    return '_job_{ttag}_{rdtag}.job'
+
 def playEOB(m1 = 10, 
             m2 = 10,
             spin1x = 0,
@@ -33,15 +40,15 @@ def playEOB(m1 = 10,
             spin2z = 0,
             eccentricity = 0,
             fMin = 40,
-            fs = 16384,
-            jobtag = '_job'):
+            fs = 16384):
 
     CMD = ConstructCMD(exe = EXE, m1 = m1, m2 = m2, 
                     spin1z = spin1z,
                     spin2z = spin2z,
                     eccentricity = eccentricity, 
                     f_min = fMin, sample_rate = fs)
-    status, data = cmd_stdout_cev(CMD, jobtag)
+    jobtag = 
+    status, data = cmd_stdout_cev(CMD, get_random_jobtag())
     if status is CEV.SUCCESS and len(data) != 0:
         return (data[:,0], data[:,1], data[:,2])
     else:
@@ -61,8 +68,7 @@ def playEOB_withAdj(m1 = 10,
                     KK = 0,
                     dSS = 0,
                     dSO = 0,
-                    dtPeak = 0,
-                    jobtag = '_job'):
+                    dtPeak = 0):
 
     CMD = ConstructCMD(exe = EXE, m1 = m1, m2 = m2, 
                     spin1z = spin1z,
@@ -71,7 +77,7 @@ def playEOB_withAdj(m1 = 10,
                     f_min = fMin, sample_rate = fs,
                     KK = KK, dSS = dSS, dSO = dSO, dtPeak = dtPeak)
 
-    status, data = cmd_stdout_cev(CMD, jobtag)
+    status, data = cmd_stdout_cev(CMD, get_random_jobtag())
     if status is CEV.SUCCESS and len(data) != 0:
         return (data[:,0], data[:,1], data[:,2])
     else:
